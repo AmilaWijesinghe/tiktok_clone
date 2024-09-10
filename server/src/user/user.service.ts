b13596d0dcf05f94bbf0786e53b6242d73e36dde
AmilaWijesinghe <amilanwijesinghe01@gmail.com>
2024-09-10 16:13:48 +0530

    update

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,26 +1,29 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserInput } from './dto/create-user.input';
-import { UpdateUserInput } from './dto/update-user.input';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class UserService {
-  create(createUserInput: CreateUserInput) {
-    return 'This action adds a new user';
+  constructor(private prisma: DatabaseService) {}
+
+  async updateProfile(
+    userId: number,
+    data: { fullname?: string; bio?: string; image?: string },
+  ) {
+    return this.prisma.user.update({
+      where: { id: userId },
+      data: {
+        fullname: data.fullname,
+        bio: data.bio,
+        image: data.image,
+      },
+    });
   }
 
-  findAll() {
-    return `This action returns all user`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserInput: UpdateUserInput) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async getUsers() {
+    return this.prisma.user.findMany({
+      include: {
+        posts: true,
+      },
+    });
   }
 }
